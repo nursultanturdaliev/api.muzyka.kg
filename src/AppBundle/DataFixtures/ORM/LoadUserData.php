1d579a70344f974983ea163bc9e9168184071c90
Nursultan Turdaliev <nursultan2010@gmail.com>
2016-11-25 13:17:46 +0300

    Added Material Design.

---
+++ src/AppBundle/DataFixtures/ORM/LoadUserData.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: nursultan
+ * Date: 11/25/16
+ * Time: 2:28 AM
+ */
+
+namespace AppBundle\DataFixtures\ORM;
+
+
+use AppBundle\Entity\Song;
+use Application\Sonata\UserBundle\Entity\User;
+use Doctrine\Common\DataFixtures\FixtureInterface;
+use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+
+class LoadUserData implements FixtureInterface, OrderedFixtureInterface
+{
+
+    /**
+     * Load data fixtures with the passed EntityManager
+     *
+     * @param ObjectManager $manager
+     */
+    public function load(ObjectManager $manager)
+    {
+        $userAdmin = new User();
+        $userAdmin->setUsername('admin');
+        $userAdmin->setSuperAdmin(true);
+        $userAdmin->setEmail('nursultan2010@gmail.com');
+        $userAdmin->setPassword('admin');
+
+        $manager->persist($userAdmin);
+        $manager->flush();
+    }
+
+    /**
+     * Get the order of this fixture
+     *
+     * @return integer
+     */
+    public function getOrder()
+    {
+        return 0;
+    }
+}
