01e94d9df2b5e6924e639f65f9147e119ef1c45b
Nursultan Turdaliev <nursultan2010@gmail.com>
2017-02-10 21:19:35 +0100

    Added api doc.

--- src/LyricsBundle/Controller/API/ArtistController.php
+++ src/LyricsBundle/Controller/API/ArtistController.php
@@ -3,6 +3,8 @@
 namespace LyricsBundle\Controller\API;
 
 use AppBundle\Controller\API\ApiController;
+use Nelmio\ApiDocBundle\Annotation\ApiDoc;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\JsonResponse;
 
@@ -15,6 +17,16 @@ class ArtistController extends ApiController
 {
     /**
      * @Route("/{offset}/{limit}/", requirements={"offset"="\d+", "limit"="\d+"})
+     * @Method("GET")
+     * @ApiDoc(
+     *     section="Lyrics",
+     *     resource=true,
+     *     description="Get artists with all lyrics offset by {offset} and limit by {limit}",
+     *     requirements={
+     *          {"name"="offset", "datatype"="integer", "requirement"="\d+", "description"="Offset"},
+     *          {"name"="offset", "datatype"="integer", "requirement"="\d+", "description"="Limit"}
+     *    }
+     * )
      * @param $offset int
      * @param $limit int
      * @return \Symfony\Component\HttpFoundation\Response
@@ -32,6 +44,12 @@ class ArtistController extends ApiController
 
     /**
      * @Route("/info")
+     * @Method("GET")
+     * @ApiDoc(
+     *     section="Lyrics",
+     *     resource=true,
+     *     description="Get lyrics Info"
+     * )
      */
     public function infoAction()
     {
