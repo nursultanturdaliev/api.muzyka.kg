cc655c61cec20bc1666e28ec46fb48c8254b7edf
Nursultan Turdaliev <nursultan2010@gmail.com>
2017-09-20 22:26:11 +0200

    Added deployment script to production.

---
+++ setup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+export SYMFONY_ENV=prod
+
+chmod -R 777 app/cache app/logs
+
+if [ ! -f "composer.phar" ]; then
+    # get latest composer.phar
+    curl -s http://getcomposer.org/installer | php
+fi
+
+php composer.phar install --no-dev --optimize-autoloader
+php composer.phar dump-autoload --optimize --no-dev --classmap-authoritative
+php app/console assets:install --env=prod
+php app/console cache:clear --env=prod --no-debug --no-warmup
+
+chmod -R 777 app/cache app/logs
